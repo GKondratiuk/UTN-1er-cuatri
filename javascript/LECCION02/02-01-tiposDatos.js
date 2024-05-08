640f9429b4dffdc2616a76a017ff2568925b0a91
GKondratiuk <kondra.guille@gmail.com>
2024-05-07 21:44:08 -0300

    leccion02 js y similitudes

--- javascript/LECCION02/02-01-tiposDatos.js
+++ javascript/LECCION02/02-01-tiposDatos.js
@@ -41,3 +41,24 @@ class Persona{
     }
 }
 console.log(typeof Persona);
+
+//TIPO DE DATO UNDEFINED
+var x;
+console.log(typeof x);
+
+x = undefined;
+console.log(x)
+//TIPO DE DATO NULL
+//null significa ausencia de valor 
+var y = null; //null no es un tipo de dato, pero su origen es obect
+console.log(typeof y)
+
+//Tipo de dato array y Empty array 
+
+var autos = ["Citroen","Audi","BMW","Ford"]
+console.log(autos)
+console.log(typeof autos) //es de tipo objeto
+
+var z = ""; //empty string - cadena vacía
+console.log(z);
+console.log(typeof z);
