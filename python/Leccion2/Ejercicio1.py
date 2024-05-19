5e121df53aaf1e85f1e6fb5059e35c695b0f155d
GKondratiuk <kondra.guille@gmail.com>
2024-05-19 11:01:23 -0300

    otro commit

--- python/Leccion2/Ejercicio1.py
+++ python/Leccion2/Ejercicio1.py
@@ -1,6 +1,14 @@
+'''
 a = float(input("Digite numero A: "))
 b = float(input("Digite numero B: "))
 c = float(input("Digite numero C: "))
 
 resultado = a ** 3 * (b ** 2 - 2 * a * c ) / (2*b)
 print(f"El resultado es {resultado}")
+'''
+
+a = float(input("Digite numero A: "))
+b = float(input("Digite numero B: "))
+
+resultado = ((3+5*8)<3 and ((6/3*4)+2<2)) or (a>b)
+print(f"el resultado es {resultado}")
